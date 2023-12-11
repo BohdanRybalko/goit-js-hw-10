0b8edb1c096bcc7166ea49d74d2e4ee99e64e657
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2023-12-11 11:29:25 +0200

    save 6

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
 import SlimSelect from 'slim-select';
-import Notiflix from 'notiflix';
 
-const catInfoDiv = document.querySelector('.cat-info');
-const catImage = document.getElementById('catImage');
-const catBreed = document.getElementById('catBreed');
-const catDescription = document.getElementById('catDescription');
-const catTemperament = document.getElementById('catTemperament');
-const loader = document.getElementById('loader');
-const error = document.getElementById('error');
+const catInfo = document.querySelector('.cat-info');
+const catImage = document.getElementById('cat-image');
+const breedName = document.getElementById('breed-name');
+const description = document.getElementById('description');
+const temperament = document.getElementById('temperament');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
 
 axios.defaults.headers.common['x-api-key'] =
   'live_kJg81KMXtrfRvKqG7wd1EroCyyJXxU1vQ817RuyIzvG5Ou4AYf1idsnGq61UeN7m';
@@ -23,43 +22,62 @@ export function fetchBreeds() {
 }
 
 export function fetchCatByBreed(breedId) {
+  const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
   return axios
-    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-    .then(response => response.data[0])
+    .get(url)
+    .then(response => response.data)
     .catch(error => {
       throw error;
     });
 }
-
 const breedSelect = new SlimSelect({
-  select: '#breedSelect',
+  select: '#breed-select',
   placeholder: 'Select a breed',
 });
 
-breedSelect.setData(
-  fetchBreeds().then(breeds =>
-    breeds.map(breed => ({ text: breed.name, value: breed.id }))
-  )
-);
+fetchBreeds()
+  .then(breeds => {
+    breedSelect.setData(
+      breeds.map(breed => ({ text: breed.name, value: breed.id }))
+    );
+  })
+  .catch(() => {
+    showError();
+  });
 
-breedSelect.on('change', info => {
-  const selectedBreedId = info.value();
-  loader.style.display = 'block';
-  catInfoDiv.style.display = 'none';
-  error.style.display = 'none';
-
-  fetchCatByBreed(selectedBreedId)
-    .then(catData => {
-      catImage.src = catData.url;
-      catBreed.textContent = `Breed: ${catData.breeds[0].name}`;
-      catDescription.textContent = `Description: ${catData.breeds[0].description}`;
-      catTemperament.textContent = `Temperament: ${catData.breeds[0].temperament}`;
-      catInfoDiv.style.display = 'block';
-    })
-    .catch(() => {
-      error.style.display = 'block';
-    })
-    .finally(() => {
-      loader.style.display = 'none';
-    });
+breedSelect.slim.addEventListener('change', function () {
+  const selectedBreedId = this.selected();
+  if (selectedBreedId) {
+    showLoader();
+    fetchCatByBreed(selectedBreedId)
+      .then(catData => {
+        displayCatInfo(catData[0]);
+      })
+      .catch(() => {
+        showError();
+      })
+      .finally(() => {
+        hideLoader();
+      });
+  }
 });
+
+function displayCatInfo(cat) {
+  catInfo.style.display = 'block';
+  catImage.src = cat.url;
+  breedName.textContent = `Breed: ${cat.breeds[0].name}`;
+  description.textContent = `Description: ${cat.breeds[0].description}`;
+  temperament.textContent = `Temperament: ${cat.breeds[0].temperament}`;
+}
+
+function showLoader() {
+  loader.style.display = 'block';
+}
+
+function hideLoader() {
+  loader.style.display = 'none';
+}
+
+function showError() {
+  error.style.display = 'block';
+}
