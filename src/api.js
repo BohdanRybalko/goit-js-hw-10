fac8f6e4c419278999b4970dc3eab2b62d75e1a7
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2023-12-12 10:49:14 +0200

    save api

--- src/api.js
+++ src/api.js
@@ -3,21 +3,21 @@ import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_kJg81KMXtrfRvKqG7wd1EroCyyJXxU1vQ817RuyIzvG5Ou4AYf1idsnGq61UeN7m';
 
-export function fetchBreeds() {
-  return axios
-    .get('https://api.thecatapi.com/v1/breeds')
-    .then(response => response.data)
-    .catch(error => {
-      throw error;
-    });
+export async function fetchBreeds() {
+  try {
+    const response = await axios.get('https://api.thecatapi.com/v1/breeds');
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 }
 
-export function fetchCatByBreed(breedId) {
+export async function fetchCatByBreed(breedId) {
   const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
-  return axios
-    .get(url)
-    .then(response => response.data)
-    .catch(error => {
-      throw error;
-    });
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 }
